ac8702d46a1873a92bc034211200a92af6613943
kobyvang <80074281+kobyvang@users.noreply.github.com>
2021-07-12 16:00:45 -0500

    Add files via upload

--- frontendmusic/music-app/src/component/Navigation.js
+++ frontendmusic/music-app/src/component/Navigation.js
@@ -1,1 +1,14 @@
+import { NavLink } from "react-router-dom";
 
+
+
+
+ const Navigation = () => (
+      <nav>
+        <ul>
+          <li><NavLink to='/'>Home</NavLink></li>
+          <li><NavLink to='/about'>About</NavLink></li>
+          <li><NavLink to='/contact'>Contact</NavLink></li>
+        </ul>
+      </nav>
+    );
